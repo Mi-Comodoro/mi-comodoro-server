bc4a962a44ad1c07abd85f054e02882554f22591
Miguel Alvarez <ing.migueljalvarez@gmail.com>
2026-05-15 07:54:52 -0500

    feat(groups): add GroupsModule with full CRUD endpoints

--- src/modules/api/modules/groups/groups.module.ts
+++ src/modules/api/modules/groups/groups.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { GroupsService } from './application/groups.service';
+import { GroupsController } from './infrastructure/controller/groups.controller';
 import { GroupMemberEntity } from './infrastructure/database/entities/group-member.entity';
 import { UserGroupEntity } from './infrastructure/database/entities/user-group.entity';
 import { GroupMemberRepositoryImpl } from './infrastructure/repositories/group-member.repository.impl';
@@ -9,6 +11,7 @@ import { UserGroupRepositoryImpl } from './infrastructure/repositories/user-grou
 @Module({
   imports: [TypeOrmModule.forFeature([UserGroupEntity, GroupMemberEntity])],
   providers: [
+    GroupsService,
     {
       provide: 'UserGroupRepository',
       useClass: UserGroupRepositoryImpl,
@@ -18,6 +21,7 @@ import { UserGroupRepositoryImpl } from './infrastructure/repositories/user-grou
       useClass: GroupMemberRepositoryImpl,
     },
   ],
+  controllers: [GroupsController],
   exports: ['UserGroupRepository', 'GroupMemberRepository'],
 })
 export class GroupsModule {}
